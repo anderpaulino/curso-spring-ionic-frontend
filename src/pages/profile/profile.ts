4dc8c6e6c034e67d6aa548225513e48be4ee9dfa
Anderson Paulino <anderpaulino@gmail.com>
2018-05-11 12:42:03 -0400

    Usando cordova para tirar foto com a câmera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,13 +4,8 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api-config';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -21,8 +16,13 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
 
+  picture: string;
+
+  cameraOn: boolean = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public storage: StorageService, public clienteService: ClienteService) {
+              public storage: StorageService, public clienteService: ClienteService,
+              public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -50,6 +50,21 @@ export class ProfilePage {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
     },
     error => {})
+  }
+  getCameraPicture(){
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
   }
 
 }
