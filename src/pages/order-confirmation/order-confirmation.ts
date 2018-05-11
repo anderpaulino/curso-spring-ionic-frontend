ba52efd931997d9145a25dfc36610d9f890f80cc
Anderson Paulino <anderpaulino@gmail.com>
2018-05-11 10:33:16 -0400

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 /**
  * Generated class for the OrderConfirmationPage page.
@@ -29,7 +30,8 @@ export class OrderConfirmationPage {
   endereco: EnderecoDTO;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public cartService: CartService, public clienteService: ClienteService) {
+              public cartService: CartService, public clienteService: ClienteService,
+              public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
     this.cartItems = this.cartService.getCart().items;
@@ -45,6 +47,24 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+    },
+    error => {
+      if(error.status == 403){
+        this.navCtrl.setRoot('HomePage');
+      }
+    })
+
   }
 
   findEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO {
