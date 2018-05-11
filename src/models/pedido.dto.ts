889176a3df6746666295bc12cc6213c25631d32f
Anderson Paulino <anderpaulino@gmail.com>
2018-05-11 08:57:39 -0400

    Armazenamento dos dados do pedido

---
+++ src/models/pedido.dto.ts
@@ -0,0 +1,11 @@
+import { RefDTO } from "./ref.dto";
+import { ItemPedidoDTO } from "./item-pedido.dto";
+
+
+
+export interface PedidoDTO{
+  cliente: RefDTO;
+  enderecoDeEntrega: RefDTO;
+  pagamento: RefDTO;
+  itens: ItemPedidoDTO[];
+}
--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { StorageService } from '../../services/storage.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -11,26 +13,42 @@ import { ClienteService } from '../../services/domain/cliente.service';
 })
 export class PickAddressPage {
 
+  pedido: PedidoDTO;
+
   items: EnderecoDTO[];
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public storage: StorageService, public clienteService: ClienteService) {
+              public storage: StorageService, public clienteService: ClienteService,
+              public cartService: CartService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-      this.clienteService.findByEmail(localUser.email)
-      .subscribe(response => {
-        this.items = response['enderecos'];
-      },
-    error => {
-      if(error.status == 403){
-        this.navCtrl.setRoot('HomePage');
-      }
-    });
-  }else{
-    this.navCtrl.setRoot('HomePage');
+        this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: response['id'],
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(x => {return {quantidade:x.quantidade, produto: {id: x.produto.id}}})
+          };
+        },
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }else{
+      this.navCtrl.setRoot('HomePage');
+    }
   }
+
+  nextPage(item: EnderecoDTO){
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 
 }
